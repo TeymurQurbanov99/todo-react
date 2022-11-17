64a62f9a0deeb989cb944372beba1518f007869b
TeymurQurbanov99 <teyqur@gmail.com>
2022-11-17 13:16:24 +0400

    put layouts in design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+  const [task, setTask] = useState<string>("")
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-        <input type="text" placeholder="Task..." />
-        <input type="number" placeholder="Deadline (in days)" />
+        <input type="text" placeholder="Task..."/>
+        <div> <input type="number" placeholder="Deadline (in days)" /></div> 
         </div>
         <button>Add Task</button>
         
