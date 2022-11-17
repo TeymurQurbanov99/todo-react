bb49d533ff031b680a299bb8239254f6214a8faa
TeymurQurbanov99 <teyqur@gmail.com>
2022-11-17 13:31:01 +0400

    added a bit of functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React, {FC, useState} from 'react';
+import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
 
-  const [task, setTask] = useState<string>("")
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "task"){
+      setTask(event.target.value)
+    } else {
+    setDeadline(Number(event.target.value))
+    }
+  };
 
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-        <input type="text" placeholder="Task..."/>
-        <div> <input type="number" placeholder="Deadline (in days)" /></div> 
+        <input type="text" placeholder="Task..." name="task" onChange={handleChange}/>
+        <div> <input type="number" placeholder="Deadline (in days)" name="deadline" onChange={handleChange}/></div> 
         </div>
         <button>Add Task</button>
         
